addc8b31558b19ffbaa9751479df83ae3b7d3041
Muyon Rivera, Christian Alfredo <cmuyonri@pichincha.com>
2022-03-07 14:01:34 -0500

    import RouterFunction

--- msa-chapter-webflux/src/main/java/com/pichincha/configuration/RouterConfiguration.java
+++ msa-chapter-webflux/src/main/java/com/pichincha/configuration/RouterConfiguration.java
@@ -1,9 +1,11 @@
 package com.pichincha.configuration;
 
+
 import com.pichincha.handler.PokemonHandler;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.ServerResponse;
 
 import static org.springframework.web.reactive.function.server.RouterFunctions.route;
