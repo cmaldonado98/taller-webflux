ccfdf0578461b505c148f3ad727317c4ba6427e2
Christian Alfredo Muyon Rivera <cmuyonri@pichincha.com>
2022-04-08 13:33:07 -0500

    Adjust on pom files and delete src folder

--- msa-chapter-webflux/src/main/java/com/pichincha/helper/WebClientHelper.java
+++
@@ -1,5 +0,0 @@
-package com.pichincha.helper;
-
-public class WebClientHelper {
-
-}
--- msa-chapter-webflux/src/main/java/com/pichincha/repository/PokemonRepository.java
+++
@@ -1,9 +0,0 @@
-package com.pichincha.repository;
-
-import com.pichincha.domain.dto.pokemon.response.Pokemon;
-import reactor.core.publisher.Mono;
-
-public interface PokemonRepository {
-
-    Mono<Pokemon> getPokemons();
-}
