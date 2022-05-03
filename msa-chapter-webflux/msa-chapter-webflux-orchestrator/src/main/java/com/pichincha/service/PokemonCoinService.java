3db1faec2aa8bf09ad45e2db0f93b70e3ff0eaa1
carmaldo <carmaldo@pichincha.com>
2022-05-03 16:41:17 -0500

    Cambios plantilla taller webflux

--- msa-chapter-webflux/msa-chapter-webflux-orchestrator/src/main/java/com/pichincha/service/PokemonCoinService.java
+++
@@ -1,8 +0,0 @@
-package com.pichincha.service;
-
-import com.pichincha.model.entity.PokemonCoin;
-import reactor.core.publisher.Mono;
-
-public interface PokemonCoinService {
-  public Mono<PokemonCoin> getPokemonCoin(String id);
-}
